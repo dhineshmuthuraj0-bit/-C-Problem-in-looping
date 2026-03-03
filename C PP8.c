149450ce4a3a0567fb66ac42c3f440c71280f4e7
Dhinesh M <dhineshmuthuraj0@gmail.com>
2026-03-03 10:13:01 +0530

    Create C PP8.c

---
+++ C PP8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n = 5;
+    for(int i=n; i>=1; i--) {
+        for(int j=0; j<i; j++) {
+            printf("%c", 'A' + j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
